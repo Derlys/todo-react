045f4546ddfccad39c5a8b408aad18d1b9521081
Derlys Dominguez <derlys275@gmail.com>
2024-05-29 12:40:11 +0300

    feat: create file button-add

---
+++ src/features/button-add.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function ButtonAdd(props){
+    return(
+        <div className={''}>
+            <button>Add task</button>
+        </div>
+    );
+}
+
+export default ButtonAdd;
