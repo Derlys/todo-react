44b2d7a2356d597fc81fed1f7d1ecad47f51cad4
Derlys Dominguez <derlys275@gmail.com>
2024-05-29 16:34:35 +0300

    feat: logic of button-add

--- src/features/button-add.jsx
+++ src/features/button-add.jsx
@@ -1,11 +1,19 @@
-import React from "react"
+import React, {useState} from "react"
+import Form from "../components/Form"
 
 function ButtonAdd(props){
+const [show,setShow] = useState(false)
+    function handleForm(){
+    setShow((s)=>!s)
+    }
     return(
         <div className={''}>
-            <button>Add task</button>
+            <button color={'primary'} className={'px-4'} onClick={handleForm}>Add task</button>
+            {show ? <Form/> : null}
         </div>
+
     );
 }
+
 
 export default ButtonAdd;
